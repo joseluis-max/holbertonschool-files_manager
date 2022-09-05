102a56ab8f4edc71a0836221aff821c59c020aca
Joseph Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-09-05 12:48:08 -0500

    Add: redis client instance

---
+++ utils/redis.js
@@ -0,0 +1,32 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.clientGetAsync = promisify(this.client.get).bind(this.client);
+    this.client.on('error', (err) => console.log(err));
+    this.client.on('connect', () => console.log());
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const res = await this.clientGetAsync(key);
+    return res;
+  }
+
+  async set(key, value, duration) {
+    return this.client.setex(key, value, duration);
+  }
+
+  async del(key) {
+    return this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
